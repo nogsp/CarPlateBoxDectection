e3609d4d810853b5e08e4c4827905d70cf6dd94c
Riei-Joaquim <rjmr@cin.ufpe.br>
2021-08-22 11:39:53 -0300

    (feat): adding text recognition

--- plate.py
+++ plate.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import imutils
 import glob
+import easyocr
 from matplotlib import pyplot as plt
 from numpy.fft import fft
 #from scipy.ndimage.filters import gaussian_filter
@@ -10,8 +11,8 @@ from graham import graham_scan
 from graham import pt
 
 def highligthPixels(img):
-    for i in range(512):
-        for j in range(512):
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
             if(img[i][j] > 128):
                 img[i][j] = max(128, img[i][j] - img[i][j]*0.15)
             else:
@@ -53,20 +54,32 @@ def normalizer(img):
     img = img.astype(np.uint8)
     return img
 
-def getcontours(img, img_back):
+def findPlate(imgRef, frames):
+    reader = easyocr.Reader(['en'])
+    boxWithText = []
+    for f in frames:
+        x,y,w,h = f[1]
+        Cropped = imgRef[y:y+h, x:x+w]
+        ans = reader.readtext(Cropped)
+        for element in ans:
+            (_, text, _) = element
+            if len(text) > 1:
+                boxWithText.append(f[1])
+    
+    return boxWithText
+
+def getcontours(img, img_back, imgRef):
     contours = cv2.findContours(img_back.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
     contours = sorted(contours,key=cv2.contourArea, reverse = True)[:30]
-    screenCnt = None
-
+    screenRect = None
+    selectedsFrames = []
+    
     for c in contours:
         x,y,w,h = cv2.boundingRect(c)
-
-        if(w >= h):
-            cv2.rectangle(img, (x,y), (x+w,y+h),(255,0,0),2)
-
-        if((w*h >= 1500 and w*h <= 25000) and (w >= h)):
-            cv2.rectangle(img, (x,y), (x+w,y+h),(0,255,0),2)
+        
+        if((w>(h*0.75))):
+            selectedsFrames.append([w*h,[x,y,w,h]])
 
         '''
         # approximate the contour
@@ -76,7 +89,6 @@ def getcontours(img, img_back):
         #contourHull = cv2.convexHull(c)
         #cv2.drawContours(img, [c], -1, (0, 0, 255), 3)
         #cv2.drawContours(img, [c], -1, (0, 0, 255), 3)
-
         #if(len(approx) == 5 or len(approx) == 4):
         #    cv2.drawContours(img, [c], -1, (0, 0, 255), 3)
         lisandra = []
@@ -104,18 +116,26 @@ def getcontours(img, img_back):
                 cv2.drawContours(img, [approx], -1, (255, 0, 0), 3)
                 #break
         '''
+    selectedsFrames = sorted(selectedsFrames, key=lambda x: x[0])
+    screenRect = findPlate(imgRef, selectedsFrames)
 
-    if screenCnt is None:
+    for select in selectedsFrames:
+        x,y,w,h = select[1]
+        cv2.rectangle(imgRef, (x,y), (x+w, y+h), (0, 0, 255), 2)
+
+    if screenRect is None:
         detected = 0
         print ("No contour detected")
         
     else:
         detected = 1
-        #cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+        for b in screenRect:
+            x,y,w,h = b
+            cv2.rectangle(imgRef, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
     #print(img_back)
     #print(img_back.dtype)
-    return img, img_back
+    return imgRef, img_back
 
 def someFilters(img):
         #kernelmatrix = np.ones((3,3),np.float32)/25
@@ -131,7 +151,8 @@ def main():
     for image_title in list_images:
         # Open an treat image ###########
         img = cv2.imread(image_title, cv2.IMREAD_COLOR)
-        img = cv2.resize(img, (512, 512))
+        img = imutils.resize(img, width=512)
+        imgBase = img.copy()
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # highlight black and white points
@@ -140,7 +161,8 @@ def main():
         # sharpFilter using statistical method to sharp image
         img = sharpFilter(img)
         img = sharpFilter(img)
-
+        img = sharpFilter(img)
+        
         #img = someFilters(img)
 
         # Using FFT to detect border
@@ -150,9 +172,10 @@ def main():
         img_back = normalizer(img_back)
 
         #finding contours in the image
-        img, img_back = getcontours(img, img_back)
+        img, img_back = getcontours(img, img_back, imgBase)
+        img_back = cv2.cvtColor(img_back,cv2.COLOR_GRAY2RGB)
         
-        img_and_magnitude = np.concatenate((img, img_back), axis=1)
+        img_and_magnitude = np.concatenate((imgBase, img_back), axis=1)
 
         cv2.imshow('image_title', img_and_magnitude)
         cv2.waitKey(0)
